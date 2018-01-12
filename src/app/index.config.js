2ac1b5dfc622860fee07eef4cdfeb17111f09725
alejunco <ale911115@gmail.com>
2018-01-12 11:48:09 -0500

    update angular-material npm package

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -11,16 +11,16 @@
     $logProvider.debugEnabled(true);
 
     //angular material
-    $mdThemingProvider.theme('myTheme')
-      .primaryPalette('blue', {
-        'default': '400', 
-      })
-      .accentPalette('green', {
-        'default': '400' // use shade 200 for default, and keep all other shades the same
-      });
-      
-      $mdThemingProvider.setDefaultTheme('myTheme');
-      // .backgroundPalette('brown');
+    $mdThemingProvider.theme('altTheme')
+      .primaryPalette('purple')
+    // $mdThemingProvider.theme('altTheme')
+    //   .primaryPalette('purple')
+    //   .accentPalette('orange', {
+    //     'default': '400'
+    //   });
+
+    $mdThemingProvider.setDefaultTheme('altTheme');
+    // .backgroundPalette('brown');
   }
 
 })();
